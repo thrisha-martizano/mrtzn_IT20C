444a3a11f1e75e3b81be087da565de74002c4b3f
thrisha-martizano <martizanothrisha@gmail.com>
2025-11-17 14:30:11 +0800

    Addded if else statement

--- cs.js
+++ cs.js
@@ -5,3 +5,29 @@ function legalAge(age){
         console.log("You are not an adult");
     }
 }
+
+legalAge(50);
+
+function computeGrade(grade){
+
+    if(grade >= 94.5){
+        console.log("1");
+
+    } else if (grade >=91.5){
+        console.log("1.25");
+
+    } else if (grade >=89.5){
+        console.log("1.5");
+
+    } else if (grade >=86.5){
+        console.log("1.75");
+
+    } else if (grade >=84.5){
+        console.log("2");
+
+    } else {
+        console.log("Not within the transmutation");
+    }
+}
+
+computeGrade(85);
